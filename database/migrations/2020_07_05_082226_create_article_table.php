ebf34a7ff8fe6e7aac622e1c17f5f80e193b2bc5
fahriefatihah <fahriefatihah@gmail.com>
2020-07-05 15:40:45 +0700

    migratedone

--- database/migrations/2020_07_05_082226_create_article_table.php
+++ database/migrations/2020_07_05_082226_create_article_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArticleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('article', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username');
+            $table->string('judul');
+            $table->string('isi');
+            $table->string('slug');
+            $table->string('tag');
             $table->timestamps();
         });
     }
@@ -27,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('article');
     }
 }
